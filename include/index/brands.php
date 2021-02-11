cc714fc6bd74220d208ce243d8367add45de00db
VladProfitkit <vlad@profitkit.ru>
2021-02-11 17:14:26 +0300

    Подгрузка

--- include/index/brands.php
+++ include/index/brands.php
@@ -2,57 +2,146 @@
 	"bitrix:news.list",
 	"brands",
 	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"ADD_STYLES_FOR_MAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"BRAND_CODE" => "BRAND",
+		"BRAND_PAGE" => "/brands/",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CATALOG_BRAND_CODE" => "BRAND",
+		"CATALOG_FILTER_NAME" => "arrFilter",
+		"CATALOG_IBLOCK_ID" => "7",
+		"CHECK_DATES" => "Y",
+		"COUNT_ITEMS" => "0",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "N",
 		"DISPLAY_DATE" => "Y",
 		"DISPLAY_NAME" => "Y",
 		"DISPLAY_PICTURE" => "Y",
 		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"AJAX_MODE" => "N",
-		"IBLOCK_TYPE" => "presscenter",
-		"IBLOCK_ID" => "3",
-		"NEWS_COUNT" => "10000",
-		"SORT_BY1" => "ACTIVE_FROM",
-		"SORT_ORDER1" => "DESC",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER2" => "ASC",
-		"FILTER_NAME" => "",
-		"FIELD_CODE" => array(),
-		"PROPERTY_CODE" => array("BRAND"),
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "Y",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"ADD_SECTIONS_CHAIN" => "Y",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"PARENT_SECTION" => "",
-		"PARENT_SECTION_CODE" => "",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "36000000",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"PAGER_TEMPLATE" => ".default",
 		"DISPLAY_TOP_PAGER" => "N",
-		"DISPLAY_BOTTOM_PAGER" => "N",
-		"PAGER_TITLE" => "Новости",
-		"PAGER_SHOW_ALWAYS" => "N",
+		"FIELD_CODE" => array(),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "3",
+		"IBLOCK_TYPE" => "presscenter",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"NEWS_COUNT" => "10000",
 		"PAGER_DESC_NUMBERING" => "N",
 		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
 		"PAGER_SHOW_ALL" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"AJAX_OPTION_HISTORY" => "N",
-		"BRAND_PAGE" => "/brands/",
-		"BRAND_CODE" => "BRAND",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array("BRAND"),
 		"SECTIONS_CODE" => "SECTIONS",
-		"CATALOG_FILTER_NAME" => "arrFilter",
-		"CATALOG_IBLOCK_ID" => "7",
-		"CATALOG_BRAND_CODE" => "BRAND",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
 		"SHOW_BOTTOM_SECTIONS" => "Y",
-		"ADD_STYLES_FOR_MAIN" => "Y",
-		"COUNT_ITEMS" => "0",
-	),
-false
-);?>
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC"
+	)
+);?> <br>
+ <br>
+<p style="text-align: justify;">
+	 Сделать эффектный дизайн ногтей теперь можно дома, даже не имея художественных навыков. Достаточно купить слайдеры для ногтей в нашем онлайн магазине. В нашей коллекции собраны разнообразные слайдеры от производителя для создания декора под любой образ. На официальном сайте можно ознакомиться с аксессуарами и купить дешево слайдер-дизайн для ногтей.
+</p>
+<p style="text-align: justify;">
+</p>
+<h2 style="text-align: justify;"> <b>Слайдер-дизайн от Lucky Rose</b> </h2>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+	 Дизайн ногтей с помощью слайдеров представляет собой технологию наклейки на пластину различных рисунков. Некоторые переводные наклейки имитируют художественную роспись, при этом для создания оригинального дизайна тратится немного времени. Сделать красивый маникюр со слайдерами может мастер без практического опыта.
+</p>
+<p style="text-align: justify;">
+</p>
+<h2 style="text-align: justify;"> <b>Преимущественная сторона slider design:</b> </h2>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+</p>
+<ul>
+	<li>интересный выбор готовых орнаментов и наборов – цветы, символы, снежинки, абстракция;</li>
+	<li>возможность использования для накладных ногтей, натурального маникюра, педикюра – хорошо держится и не оказывает вредного воздействия;</li>
+	<li>благодаря эластичной пленке использовать слайдеры можно самостоятельно, без посещения нейл-салона.</li>
+</ul>
+<p>
+</p>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+	 Если вы ищете, где купить наборы для дизайна, посетите наш интернет-магазин. У нас есть огромная коллекция слайдеров Lucky Rose с разной тематикой и узнаваемым фирменным стилем. Благодаря собственному производству слайдеров для ногтей, стоят они сравнительно недорого, посмотреть цены можно в каталоге. Наш приоритет – высокое качество печати продукции, работать с нашим материалом легко и удобно как нейл-мастерам, так и дизайнерам без опыта.
+</p>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+	 К другим нашим «плюсам» относится:
+</p>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+</p>
+<ul>
+	<li>печать слайдеров на заказ – наличие собственного оборудования сокращает сроки выполнения и отправки заказов;</li>
+	<li>доставка в Москву и другие города России.</li>
+</ul>
+<p>
+</p>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+	 Оптовики могут купить в СПб продукцию для студий и нейл-салонов на специальных условиях.
+</p>
+<p style="text-align: justify;">
+</p>
+<h2 style="text-align: justify;"> <b>Характеристика слайдеров для ногтей</b> </h2>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+	 Используемые для маникюра декоративные материалы делятся по разным критериям:
+</p>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+</p>
+<ul>
+	<li><b>По размеру и количеству изображений</b><br>
+	Это может быть аппликация, слайдер-дизайн надписи, которые закрывают только часть пластины, или сплошное покрытие.</li>
+</ul>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+</p>
+<ul>
+	<li><b>По эффекту</b><br>
+	Металлик матовый и глянцевый, объемный слайдер-дизайн, на белой и прозрачной основе. Можно купить наклейки, которые дополнят деловой, повседневный или праздничный образ.</li>
+</ul>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+</p>
+<ul>
+	<li><b>По технологии переноса</b><br>
+	Обычные наклейки, имеющие клейкий слой, вырезанные по контуру. Для покрытия всей ногтевой пластины используется термопленка, которая также вырезана по контуру пластины. Самоклейки – цельная картинка, которая повторяет контуры ногтевой пластины. Аккуратно снимается пленка и наклеивается на поверхность. Водные – сочетаются с любой нейл-технологией, подходят для мастера и для домашнего использования.</li>
+</ul>
+<p style="text-align: justify;">
+</p>
+<p style="text-align: justify;">
+	 Если используется слайдер на водной основе, нужно подготовить инструмент для обработки ногтей, лампу для сушки гель лака и емкость с теплой водой. Узор отделяется от подложки и наносится, выравнивается и покрывается финишным средством.
+</p>
+<p style="text-align: justify;">
+</p>
